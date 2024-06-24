1ca0d5e2dfbdd7524091e1f82a7ab7470f340617
leekmikan <uki.shin0917@gmail.com>
2024-06-24 16:54:57 +0900

    Add list

--- articles/want.php
+++ articles/want.php
@@ -41,18 +41,18 @@
                     <p>可能であれば出品お願いいたします。</p>
                     <p>形式：[種類]商品(買い取り予定価格　?...適正価格不明、適当)</p>
                     <ol>
+						<li>優先度高</li>
                         <li>[CD]初音ミク Project mirai こんぷりーと(6,000)</li>
-						<li>[CD]初音ミク -Project DIVA-Original Song Collection(中800　新3,200)</li>
-						<li>[CD]初音ミク -Project DIVA-X Complete Collection(2,800)</li>
-                        <li>[CD]初音ミク Project DIVA Arcade Original Song Collection Vol.3(1,000)</li>
-                        <li>[CD]股関節脱臼(1,000)</li>
-                        <li>[CD]トランジスタの道化師～Heartsnative3～(1,000)</li>
-                        <li>[CD]トラボティック・シンフォニー(1,000)</li>
-						<li>[CD]ELECTLOID feat.初音ミク(1,000)</li>
-                        <li>[CD]EXIT TUNES PRESENTS VOCALOFANTASYより後のCD (1,000～4,000)</li>
-                        <li>[CD]マジカルミライのCD 2017,2018,2020,2021,2023以外(2,000～7,000　　年によって全然違うみたい)</li>
-                        <li>[その他]ペンライト(1,000～?)</li>
 						<li>[その他]寝そべりぬいぐるみ　ビビバスミク,ダショミク以外の4人(1,500?)</li>
+						<li>[CD]股関節脱臼(1,000)</li>
+						<li>[CD]トラボティック・シンフォニー(1,000)</li>
+						<li>[CD]マジカルミライのCD 2017,2018,2020,2021,2023以外(2,000～7,000　　年によって全然違うみたい)</li>
+                        <li>[CD]トランジスタの道化師～Heartsnative3～(1,000)</li>
+						<li>[CD]ELECTLOID feat.初音ミク(1,000)</li>
+						<li>[CD]初音ミク -Project DIVA-X Complete Collection(2,800)</li>
+                        <li>[CD]EXIT TUNES PRESENTS VOCALOFANTASYより後のCD (1,000～4,000)</li>
+                        <li>[その他]ペンライト(1,000～?)</li>
+						<li>優先度低</li>
                     </ol>
 					<p>※上記以外でも、初音ミクかつ商品に重大な欠陥がなければできる限り購入します</p>
 					<p>※同時に大量出品されると買いきれないかも(学部2年のアルバイトなので)</p>
