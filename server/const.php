e176543d06d2bf6194be8f4e806d0ef0ab65f59f
leekmikan <139122626+leekmikan@users.noreply.github.com>
2025-10-06 11:43:31 +0900

    シークレットキーをすべて隠しました。
    就活でプログラムを公開するため。

--- server/const.php
+++ server/const.php
@@ -1,7 +1,8 @@
 <?php
 $FEE = 0.1; //手数料の割合.
 $POINT = 0.01;//ポイントの割合.
-$SECRET = 'sk_test_51PAkRdBl4em3MIGoiz26aQbSB0AGZ64ILqtxlmNPS6Pc2GIk7J00H4MhhuKSb2sVmWBE27y5hlzlvIkCvApk9Jtb00wjWT4vid';
+//シークレットキーをすべて隠しました.
+$SECRET = '???';
 $SQL_PASS = ''; //MySQLのパスワード
 $GENRE = ['ゲーム','音楽','映像','書籍','ぬいぐるみ','グッズ','小物','電子機器','楽器','家電','その他']; //カテゴリー 追加したら　data_change.php内の関数 Add_genre(追加した位置)　を実行すること.
 $STAT = ['未開封/新品','きれい','キズ/汚れ　少','キズ/汚れ　中','キズ/汚れ　多']; //状態　追加するときは必ず一番後ろに追加.
